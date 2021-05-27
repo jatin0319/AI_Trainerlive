e6c36652f02e014f4fce6e1aff29590e056f4c18
jatin0319 <40224216+jatin0319@users.noreply.github.com>
2021-05-27 22:58:10 +0530

    Update settings.py

--- AItrainer/settings.py
+++ AItrainer/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'static')
     ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
